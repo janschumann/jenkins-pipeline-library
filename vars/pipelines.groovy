903aa01971b28274b159e16093f39359d9331aee
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-15 20:51:46 +0100

    test pipelines

--- vars/pipelines.groovy
+++ vars/pipelines.groovy
@@ -2,13 +2,13 @@ def threeFlow(body) {
     def config = configs(body)
 
     def tag = Long.toString(new Date().time, Character.MAX_RADIX)
-
+    def builder = config.builder
 
     if (env.BRANCH_NAME.startsWith("PR-")) {
         echo 'PR Flow'
-        config.buildStrategy.build()
+        builder.build()
     } else {
         echo 'Snapshot Flow'
-        config.buildStrategy.build(push: true, tag: tag, verbose: false)
+        builder.build(push: true, tag: tag, verbose: false)
     }
 }
