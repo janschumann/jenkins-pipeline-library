2e2a3db5d33185f826c14b2e56a5f681ea3576d3
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-15 20:49:00 +0100

    test pipelines

--- vars/builders.groovy
+++ vars/builders.groovy
@@ -1,6 +1,6 @@
 import de.audibene.jenkins.pipeline.DockerBuilder
 
-def docker(Closure body) {
+def call(Closure body) {
     def config = configs(body)
     return new DockerBuilder(this, config)
 }
