6ab7cfdbd8ab23686167e5b7d7e161fb44310360
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-15 12:06:49 +0100

    test pipelines

--- vars/DockerBuildStrategy.groovy
+++ vars/DockerBuildStrategy.groovy
@@ -17,33 +17,40 @@ class MyDelegate implements Serializable {
         script.echo this
     }
 
-    def run(body) {
+//    def run(body) {
 //        script.docker.image(env.image).inside(env.args) {
 //            body()
 //        }
-    }
+//    }
 
-    def build() {
+    def build(body) {
+//        script.node('ecs') {
+//            script.stage('Prepare') {
+//                script.docker.image(env.image).pull()
+//
+//                script.deleteDir()
+//
+//                script.checkout script.scm
+//
+//                script.sh 'ls -lah'
+//            }
+////            script.stage('Build') {
+////                run {
+////                    script.sh 'gradle assemble'
+////                }
+////            }
+////            script.stage('Test') {
+////                run {
+////                    script.sh 'gradle test'
+////                }
+////            }
+//        }
+
         script.node('ecs') {
-            script.stage('Prepare') {
-                script.docker.image(env.image).pull()
-
-                script.deleteDir()
-
+            script.stage('test') {
                 script.checkout script.scm
-
-                script.sh 'ls -lah'
+                body()
             }
-//            script.stage('Build') {
-//                run {
-//                    script.sh 'gradle assemble'
-//                }
-//            }
-//            script.stage('Test') {
-//                run {
-//                    script.sh 'gradle test'
-//                }
-//            }
         }
     }
 }
