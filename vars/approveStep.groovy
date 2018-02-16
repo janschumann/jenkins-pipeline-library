f6c59f3357067c6e5eb3327a438c086ad98e72f2
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-16 11:49:32 +0100

    test pipelines

--- vars/approveStep.groovy
+++ vars/approveStep.groovy
@@ -11,7 +11,7 @@ def call(String name, Map params = [:]) {
     def approve = getApprove(name, time, unit, message, timeoutAs)
 
     if (!approve.result) {
-        currentStage.result =  'ABORTED'
+//        currentStage.result =  'ABORTED'
 //        currentBuild.result = 'ABORTED'
         throw new ApproveStepRejected("Rejected by ${approve.userName}")
     }
