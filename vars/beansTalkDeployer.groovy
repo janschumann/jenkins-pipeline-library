93889f45f2dc0038bbaf333e068bdc4bcdbcf66c
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-19 20:45:58 +0100

    rework Jenkins file

--- vars/beansTalkDeployer.groovy
+++ vars/beansTalkDeployer.groovy
@@ -3,5 +3,6 @@ import de.audibene.jenkins.pipeline.deployer.BeansTalkDeployer
 
 def call(Closure body) {
     def config = configure(body)
+    echo "beanstalk config $config"
     return new BeansTalkDeployer(this, config)
 }
