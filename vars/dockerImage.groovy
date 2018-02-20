79c3826015d2a1be48afd6db0178df18809b7447
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-02-20 11:46:37 +0100

    rework deployer

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,6 +1,6 @@
 import de.audibene.jenkins.pipeline.docker.DockerImage
 
-def call(Map params = [:], Closure body) {
+def call(Map params = [:], Closure body = {}) {
     def config = configure(params, body)
     return new DockerImage(this, config)
 }
